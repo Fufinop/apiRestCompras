c2c53d696355dfb09303f41501ff2f8345459239
Ernesto De la Rosa <ernestodelazamora@gmail.com>
2024-05-12 14:22:39 -0600

    Second Crud added

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -2,34 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Responses\ApiResponse;
+use App\Models\Marca;
+use Exception;
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class MarcaController extends Controller
 {
     public function index()
     {
-        # code
-        
+        try {
+            $marcas = Marca::all();
+            return ApiResponse::success('Lista de marcas', 200, $marcas);
+
+        } catch (Exception $e) {
+            return ApiResponse::error('Error al Obtener la Lista de Marcas: '.$e->getMessage(),500);
+        }
+
     }
 
     public function store(Request $request)
     {
-        # code
+        try {
+            $request -> validate(['nombre' => 'required|unique:marcas']);
+            $marca = Marca::create($request -> all());
+            return  ApiResponse::success('Marca Creada Exitosamente', 201, $marca);
+
+        } catch (ValidationException $e) {
+            return ApiResponse::error('Error de Validación '. $e->getMessage(), 402);
+        }
     }
 
     public function show($id)
     {
-        # code
+        try {
+            $marca = Marca::findOrFail($id);
+            return ApiResponse::success('Marca Obtenida Exitosamente', 200, $marca);
+
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error('Marca no encontrada', 404);
+        }
     }
 
     public function update(Request $request, $id)
     {
-        # code
+        try {
+            $marca = Marca::findOrFail($id);
+            $request -> validate([
+                'nombre' => ['required', Rule::unique('Marcas')->ignore($marca)]
+            ]);
+            $marca -> update($request-> all());
+            return ApiResponse::success('Marca Actualizada Exitosamente', 200, $marca);
+
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error('Marca no encontrada', 404);
+        } catch (Exception $e) {
+            return ApiResponse::error('Error de Validacion: '. $e->getMessage(), 422);
+        }
     }
 
     public function destroy($id)
     {
-        # code
+        try {
+            $marca = Marca::findOrFail($id);
+            $marca -> delete();
+            return ApiResponse::success('Marca Eliminada Exitosamente', 200);
+
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error('Marca No Encontrada', 404);
+        }
     }
 
     public function productosPorMarca($id)
